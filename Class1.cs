4225dd32793ca5e7db9faa9b5666868108ca28b7
Zeta <2855674553@qq.com>
2024-12-17 10:07:12 +0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,114 @@
+﻿using HarmonyLib;
+using UnityEngine;
+using BepInEx;
+using BepInEx.Logging;
+using System;
+using System.Collections;
+using System.Reflection;
+using System.Drawing;
+using PotionCraft.ObjectBased.UIElements.InventoryWindow;
+
+namespace ukersn
+{
+    [BepInPlugin("com.ukersn.potioncraftmods.recipebutton", "RecipeButtonFix", "1.0.0")]
+    public class RecipeButtonFixPlugin : BaseUnityPlugin
+    {
+        internal static ManualLogSource Log = null;
+        private Harmony harmony = null;
+
+        private void Awake()
+        {
+            Log = Logger;
+            Log.LogInfo("配方按钮修复插件正在加载...");
+
+            StartCoroutine(InitializePlugin());
+        }
+
+        private IEnumerator InitializePlugin()
+        {
+            yield return new WaitForSeconds(10f);
+
+            try
+            {
+                harmony = new Harmony("com.ukersn.potioncraftmods.recipebutton");
+                ApplyPatches();
+                Log.LogInfo("配方按钮修复插件已成功加载！");
+            }
+            catch (Exception ex)
+            {
+                Log.LogError(string.Format("插件初始化时发生错误: {0}", ex.Message));
+                Log.LogError(ex.StackTrace);
+            }
+        }
+
+        private void ApplyPatches()
+        {
+            try
+            {
+                harmony = new Harmony("com.ukersn.potioncraftmods.recipebutton");
+
+                // ShowSavedRecipes 补丁
+                MethodInfo originalShowSavedRecipes = AccessTools.Method(typeof(OpenRecipeBookButton), "ShowSavedRecipes");
+                MethodInfo postfixShowSavedRecipes = AccessTools.Method(typeof(OpenRecipeBookButtonPatch), "PostShowSavedRecipes");
+                harmony.Patch(originalShowSavedRecipes, postfix: new HarmonyMethod(postfixShowSavedRecipes));
+
+                Log.LogInfo("成功应用 RecipeButtonFix 补丁");
+            }
+            catch (Exception ex)
+            {
+                Log.LogError(string.Format("应用补丁时发生错误: {0}", ex.Message));
+                Log.LogError(ex.StackTrace);
+            }
+        }
+
+
+        private void OnDisable()
+        {
+            try
+            {
+                harmony?.UnpatchSelf();
+                Log.LogInfo("配方按钮修复插件已成功卸载所有补丁。");
+            }
+            catch (Exception ex)
+            {
+                Log.LogError(string.Format("卸载补丁时发生错误: {0}", ex.Message));
+            }
+        }
+    }
+    
+    [HarmonyPatch(typeof(OpenRecipeBookButton))]
+    public static class OpenRecipeBookButtonPatch
+    {
+        private static Vector2 originalColliderSize=Vector2.zero;
+
+        [HarmonyPostfix]
+        [HarmonyPatch("ShowSavedRecipes")]
+        public static void PostShowSavedRecipes(OpenRecipeBookButton __instance)
+        {
+            //RecipeButtonFixPlugin.Log.LogInfo(string.Format("检查ShowSavedRecipes"));
+            ResetColliderSize(__instance);
+        }
+
+        private static void ResetColliderSize(OpenRecipeBookButton button)
+        {
+            try
+            {
+                BoxCollider2D collider = button.GetComponent<BoxCollider2D>();
+                //RecipeButtonFixPlugin.Log.LogInfo(string.Format("检查ResetColliderSize此时按钮大小 :{0}", collider.size));
+                if (originalColliderSize == Vector2.zero) {
+                    //RecipeButtonFixPlugin.Log.LogInfo(string.Format("检查到按钮碰撞器大小未初始化"));
+                    originalColliderSize = collider.size;
+                }
+                else if (collider != null && collider.size != originalColliderSize)
+                {
+                    collider.size = originalColliderSize;
+                    RecipeButtonFixPlugin.Log.LogInfo(string.Format("Reset RecipeButton collider size to: {0}", originalColliderSize));
+                }
+            }
+            catch (Exception ex)
+            {
+                RecipeButtonFixPlugin.Log.LogError(string.Format("Error in ResetColliderSize: {0}", ex.Message));
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,23 @@
+# PotionCraft_OpenRecipeButtonFix
+
+This mod is Compatible with Potion Craft v2.0。
+
+This is a small plugin I developed myself to fix an issue where the collision box of the recipe book button grows larger as you play the game. When you click the recipe book button to open the recipe book, this plugin will check the button's collider. If it has increased in size, the plugin will reset it to its original dimensions.
+
+# Installation Instructions 
+
+1. Download and install [BepInEx_x64_5.4.22][0] from GitHub
+2. Unzip ClassLibrary1.dll to Potion Craft\BepInEx\plugins folder。
+
+# 药剂工艺 打开配方书按钮修复
+
+此Mod适用于药剂工艺v2.0版本。
+
+这是我自己编写的小插件，用于修复打开配方书的按钮随着游戏游玩而导致其碰撞器越来越大的问题，在你点击配方书按钮的打开配方书的时候，这个插件会为你检查按钮的碰撞器，如果它变大了，则会将其设置回原来的大小。
+
+# 安装说明
+
+1. 在GitHub下载并安装 [BepInEx_x64_5.4.22][0]
+2. 将ClassLibrary1.dll解压到Potion Craft\BepInEx\plugins文件夹。
+
+[0]: https://github.com/BepInEx/BepInEx/releases
